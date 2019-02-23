6596df344f6cae7f621df34d8e59d7a115a26720
Hapachino <erickwbrownfield@gmail.com>
2019-02-23 15:32:14 -0800

    rootReducer complete

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,7 @@
+const DEFAULT_STATE = {};
+
+const authReducer = (state = DEFAULT_STATE, action) => {
+  return state;
+}
+
+export default authReducer;
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,7 @@
+const DEFAULT_STATE = {};
+
+const projectReducer = (state = DEFAULT_STATE, action) => {
+  return state;
+}
+
+export default projectReducer;
