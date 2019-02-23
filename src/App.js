9eacccad7ed3bfaf8882c5dc7e629a6e3f735b37
Hapachino <erickwbrownfield@gmail.com>
2019-02-22 23:46:15 -0800

    nav bar styling complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <h1>Bulletin</h1>
+          <Navbar />          
         </div>
       </Router>
     );
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const signedIn = [
+  {
+    to: '/',
+    text: 'New Project',
+  },
+  {
+    to: '/',
+    text: 'Log Out',
+  },
+  {
+    to: '/',
+    text: 'EB',
+    class: 'btn btn-floating blue lighten-1',
+  }
+];
+
+const signedOut = [
+  {
+    to: '/',
+    text: 'Sign Up',
+  },
+  {
+    to: '/',
+    text: 'Log In',
+  }
+];
+
+function renderLinks(links) {
+  const outputLinks = links.map(link => {
+    return (
+      <li key={link.to}>
+        <NavLink to={link.to} className={link.class}>{link.text}</NavLink>
+      </li>
+    );
+  });
+
+  return (
+    <ul className="right">
+      {outputLinks}
+    </ul>
+  );
+}
+
+export default () => {
+  return (
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <Link to="/" className="brand-logo">Bulletin</Link>
+        {renderLinks(signedOut)}
+      </div>
+    </nav>
+  );
+}
