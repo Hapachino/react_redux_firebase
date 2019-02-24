511167ee93bee7c3004df832c8d8cdd8d5fa43eb
Hapachino <erickwbrownfield@gmail.com>
2019-02-23 20:55:27 -0800

    dashboard projects connected to firestore

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ProjectSummary from './ProjectSummary';
 
 export default ({ projects }) => {
-  const projectSummaries = projects.length ?
+  const projectSummaries = projects && projects.length ?
     projects.map(project => <ProjectSummary project={project} key={project.id} />) :
     '';
 
