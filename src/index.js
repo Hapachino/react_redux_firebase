93f52f9ce104eb4038dd75c82e054b60ccbb665a
Hapachino <erickwbrownfield@gmail.com>
2019-02-23 17:02:00 -0800

    think middleware complete

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './App';
 import rootReducer from './store/reducers/rootReducer';
+import think from './middleware/think';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(think));
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/middleware/think.js
@@ -0,0 +1,7 @@
+export default ({ dispatch, getState }) => next => action => {
+  if (typeof action !== 'function') {
+    return next(action);
+  }
+
+  return action(dispatch, getState);
+}
